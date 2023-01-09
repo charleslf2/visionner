bd7bf6242003f24cac1a3565e631cf874eb38c23
charleslf2 <datadevfernolf@gmail.com>
2023-01-09 09:43:48 -0800

    include subplot

--- visionner/vision.py
+++ visionner/vision.py
@@ -9,7 +9,7 @@ from rich.console import Console
 console=Console()
 
 
-def Vision(path):
+def Vision(path, train_test_split=True):
 
     isDirectory = os.path.isdir(path)
     print(isDirectory)
@@ -23,7 +23,7 @@ def Vision(path):
 
     for file_name in os.listdir(path):
         img=cv2.imread(os.path.join(path, file_name))
-        img=cv2.resize(img, (200,200))
+        img=cv2.resize(img, (200,250))
         dataset.append(img)
 
     dataset=np.array(dataset)
@@ -36,7 +36,23 @@ def Vision(path):
 
     # show the datset with matplotlib
 
-    plt.imshow(dataset[10])
+    #plt.imshow(dataset[10])
+    #plt.show()
+    for i in range(10):
+        plt.subplot(2, 5, i + 1)
+        plt.imshow(dataset[i])
+  
     plt.show()
 
+
+    # train test split
+    if train_test_split==True:
+        image_heigh_size=dataset_shape[1]
+        print("image hiegh size", image_heigh_size)
+        # reshape
+
+        #x_train=np.reshape(x_train, [-1, image_size, image_size, 3])
+        #x_test=np.reshape(x_test, [-1, image_size, image_size, 3])
+    else:
+        pass
     return dataset
