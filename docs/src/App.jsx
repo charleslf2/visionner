7b486b815955b698e51bb9c810a6a45777792db7
charleslf2 <datadevfernolf@gmail.com>
2023-01-14 08:23:37 -0800

    initialise description.jsx

--- docs/src/App.jsx
+++ docs/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../src/lib/Header'
 import styles from "../src/App.module.css"
+import Description from './lib/Description'
 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <div className={styles.container}>
 
         <Header/>
+        <Description/>
 
       </div>
 
---
+++ docs/src/lib/Description.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from "../styles/Descr.module.css"
+
+
+const Description = () => {
+  return (
+    <div className={styles.container}>
+        <p>Visionner (<span>from the french , that refer to technical looking</span>),
+        Is a python package that help you start your next computer vision project with confidence
+        </p>
+    </div>
+  )
+}
+
+export default Description
