5a4b676356070d86d71db25ccaac100d401ba454
charleslf2 <datadevfernolf@gmail.com>
2023-01-14 08:40:44 -0800

    initialize the showcase_card

--- docs/src/lib/Layout.jsx
+++ docs/src/lib/Layout.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 import styles from "../styles/Layout.module.css"
+import Showcase_card from './Showcase_card'
 
 const Layout = () => {
   return (
     <div className={styles.wrapper}>
-        Layout
+        
+        <div className={styles.container}>
+
+            <div className={styles.title}>What visionner can do for you ?</div>
+
+            <div className={styles.showcase_container}>
+
+                <div className={styles.showcase_01}>
+                    <Showcase_card/>
+
+                </div>
+
+                <div className={styles.showcase_02}>
+                    
+                </div>
+
+                <div className={styles.showcase_03}>
+                    
+                </div>
+
+            </div>
+        </div>
     </div>
+
+    
   )
 }
 
---
+++ docs/src/lib/Showcase_card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "../styles/Showcase_card.module.css"
+
+const Showcase_card = () => {
+  return (
+    <div className={styles.container}>
+        Showcase_card
+    </div>
+  )
+}
+
+export default Showcase_card
