b2fc0e5088b8ceff3f758dda2978ebde2042c731
charleslf2 <datadevfernolf@gmail.com>
2023-01-15 17:37:29 -0800

    add visualization to setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     author="Charles TCHANAKE",
     author_email="datadevfernolf@gmail.com",
     license="Apache License 2.0",
-    keywords=["AI", "Computer vision", "Data centric", "dataset", "Image"],
+    keywords=["AI", "Computer vision", "Data centric", "dataset", "Image","visualization"],
     classifiers=classifiers,
     install_requires=["numpy", "opencv-python", "matplotlib", "rich", "scikit-learn"],
     packages=find_packages()
