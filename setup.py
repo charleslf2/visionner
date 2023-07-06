d3de337f9d20f25c09c141b726211f0756f3fbe2
charleslf2 <datadevfernolf@gmail.com>
2023-07-06 09:46:22 -0700

    update

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.6' 
-DESCRIPTION = 'manipulate your image dataset easly'
+VERSION = '0.0.7' 
+DESCRIPTION = "Turn raw image dataset into numpy array ; more suitable for deep learning tasks"
 
 setup(
         name="visionner", 
         version=VERSION,
         author="charles TCHANAKE",
-        author_email="<youremail@email.com>",
+        author_email="datadevfernolf@gmail.com",
+        url="https://github.com/charleslf2/visionner",
         description=DESCRIPTION,
-        #long_description=LONG_DESCRIPTION,
+        long_description_content_type="text/markdown",
+        long_description=open("README.md","r",encoding="utf-8").read(),
         packages=find_packages(),
         install_requires=["numpy","opencv-python",
                           "matplotlib","rich"],  
-        keywords=['python'],
+        keywords=["Computer-vision",
+        "preprocessing","Images","Dataset","visionner"],
         classifiers= [
             "Development Status :: 3 - Alpha",
             "Programming Language :: Python :: 3",
